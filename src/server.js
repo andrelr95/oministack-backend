8dad2d2fcca1ddd8820c71d964c0bb2ffe524e21
Andre Ramos <andre.lopes@soaexpert.com.br>
2019-04-10 11:36:15 -0300

    Pequenos Ajustes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
-app.use(cors);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -21,7 +26,7 @@ mongoose.connect('mongodb+srv://andrelr:andrelr@study-database-pmddb.mongodb.net
 app.use((req, res, next) => {
   req.io = io;
 
-  return(next);
+  return(next());
 });
 
 app.use(express.json());
@@ -30,4 +35,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-server.listen(process.env.PORT || 3333);
+server.listen(3333);
